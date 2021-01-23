899852844b850cee36e6b0834ed3b0c682e0936b
Fazt <contact@faztweb.com>
2021-01-22 22:28:51 -0500

    commit inicial

---
+++ diseño1.php
@@ -0,0 +1,88 @@
+
+<!DOCTYPE html>
+ <html lang="es">
+ <head>
+ <meta charset="UTF-8">
+ <title>Farmacity</title>
+ <meta name="viewport" content="width=device-width,user-scalable=yes, initial-scale=1.0, maximum-scale=3.0, minimum-scale=1.0">>
+<link rel="stylesheet" href="css/estilo.css">
+<link>
+ <link href="https://file.myfontastic.com/AgpgEL5stynTCJXv2mJuD8/icons.css" rel="stylesheet">
+</head>
+<body>
+ <header class="main-header">
+ <div class="container container--flex">
+  <div class="logo-container  column column--50"></div>
+  <div class="main-header_ _contacInfo column column--50"></div>
+  </div>
+</header>
+<nav class="main-nav"
+<div class="container  container--flex"
+ <span class="icon-menu" id="btnmenu"></span>
+ <ul class="menu"id="menu">
+<li class="menu_ _item"><a href="/" class="menu_ _link menu_ _link- -select">Inicio</a>
+</li>
+<h1 class="logo">Farmacity</h1>
+
+ <li class="menu_ _item"><a href="Nosotros.html" class="menu_ _link">Nosotros</a></li>
+ <li class="menu_ _item"><a href="Galeria.html" class="menu_ _link">Galeria</a></li>
+ <li class="menu_ _item"><a href="Contacto.html" class="menu_ _link">Contacto</a></li>
+ </ul>
+ <div class="social-icon">
+ <a href="" class="social-icon_ _link"><span class="icon-facebook"></span></a>
+  <a href="" class="social-icon_ _link"><span class="icon-twitter"></span></a>
+  <a href="" class="social-icon_ _link"><span class="icon-mail"></span></a>
+ </div>
+  </div>
+</nav>
+<section class="banner">
+<img src="img/banner.jpeg" alt="" class="banner_ _img">
+<div class="banner_ _content">Los mejores productos lo encuentras aqui</div>
+</section>
+<main class="main">
+  <section class="group group--color">
+    <div class="container">
+      <h2 class="main_ _title">Bienvenida a su Farmacity</h2>
+      <p class= "main_ _txt">vjvvvv </p>
+    </div>
+  </section>
+  <section class="group main_ _about_ _description">
+    <div class="container container- -flex">
+    <div class="column column- -50">
+    <img src="img/food1.jpg" alt="">
+  </div>
+  <div class="column column- -50">
+  <h3 class= "column_ _title">Expertise in industrial solution </h3>
+  <p class="column_ _txt">loream dsahaha </p>
+  <a href="" class="btn btn--contact">Contact</a>
+</div>
+</div>
+</section>
+<section class="group today-special">
+<h2 class="today-special_ _title">Especial de hoy </h2>
+<div class="container container_ _flex">
+<div class="column column- -50-25">
+<img src="img/plate1.jpg" alt="" class="today-special_ _img">
+<div class="today-special_ _title">Especial1</div>
+<div class="today-special_ _price">S\30.00</div>
+</div>
+<div class="column column- -50-25">
+<img src="img/plate3.jpeg" alt="" class="today-special_ _img">
+<div class="today-special_ _title">Especial2</div>
+<div class="today-special_ _price">S\10.00</div>
+</div>
+<div class="column column- -50-25">
+<img src="" alt="" class="today-special_ _img">
+<div class="today-special_ _title"></div>
+<div class="today-special_ _price"></div>
+</div>
+<div class="column column- -50-25">
+<img src="" alt="" class="today-special_ _img">
+<div class="today-special_ _title"></div>
+<div class="today-special_ _price"></div>
+</div>
+</div>
+
+</section>
+</body>
+</html>
